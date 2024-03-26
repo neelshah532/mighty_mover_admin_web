be4fd2ae8867fc036e9773aa1e46b717c1a4fa1d
NeelSolguruz <neels@solguruz.com>
2024-03-26 11:23:46 +0530

    develop setupt with routes

--- src/components/pages.tsx
+++ src/components/pages.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
-import Home from '../pages/Home';
+// import Home from '../pages/Home';
+import Admin from '../pages/admin';
 
 function Pages() {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
-            <Route path="/home" element={<Home />} />
+            <Route path="/admin" element={<Admin />} />
             <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
     );

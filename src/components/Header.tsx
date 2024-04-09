9a897f7fd94b71968fe16dca77c61ba10406f9da
NeelSolguruz <neels@solguruz.com>
2024-04-09 18:46:15 +0530

    solve merging issue

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Tooltip, Flex, Modal, Upload,  Avatar, Input, Button, message} from 'antd';
+import { Tooltip, Flex, Modal, Upload, Avatar, Input, Button, message } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import { PlusOutlined } from '@ant-design/icons';
 import { IoMenu } from 'react-icons/io5';
@@ -156,15 +156,15 @@ export default function Header_page() {
     return (
         <div className="w-full sticky top-0 z-10">
             {/* <Header  className="z-10 w-full bg-white shadow-sm shadow-gray-400 border-2 border-red-500"> */}
-                <Flex  justify="flex-end" className='bg-white shadow-md'>   
-                    <Flex className='h-16'  gap="small" align='center'>
-                        {/* <Avatar
+            <Flex justify="flex-end" className="bg-white shadow-md">
+                <Flex className="h-16" gap="small" align="center">
+                    {/* <Avatar
                                         src={pic || <UserOutlined />}
                                         icon={!pic ? <FaUser /> : undefined}
                                         className="rounded-full"
                                     /> */}
-                            
-                                {/* <Button
+
+                    {/* <Button
                                     className="text-white font-semibold bg-black text-xl text-center mt-5"
                                     onClick={() => setIsModalOpen(true)}
                                 >
@@ -181,40 +181,42 @@ export default function Header_page() {
                                         />
                                     )}
                                 </Button> */}
-                    <div className='flex items-center gap-3 mr-1'>
-                    <Tooltip
-                        title={
-                            toggle1 ? (
-                                <div className="flex  items-center">
-                                    {name} <img src={imageUrl} width={50} height={50} alt="avatar" />
-                                </div>
-                            ) : (
-                                <div>{'ADMIN'}</div>
-                            )
-                        }
-                    >
-                                <div className=''>
+                    <div className="flex items-center gap-3 mr-1">
+                        <Tooltip
+                            title={
+                                toggle1 ? (
+                                    <div className="flex  items-center">
+                                        {name} <img src={imageUrl} width={50} height={50} alt="avatar" />
+                                    </div>
+                                ) : (
+                                    <div>{'ADMIN'}</div>
+                                )
+                            }
+                        >
+                            <div className="">
                                 {fileList.length === 0 ? (
                                     <Avatar
-                                        className=''
+                                        className=""
                                         size="large"
                                         icon={<UserOutlined />}
                                         alt="avatar"
                                         onClick={() => setIsModalOpen(true)}
                                     />
-                                ) : (<Avatar
-                                    className=""
-                                    size="large"
-                                    src={pic || <UserOutlined />}
-                                    icon={!pic ? <FaUser /> : undefined}
-                                    alt="avatar"
-                                    onClick={() => setIsModalOpen(true)}
-                                />)}
-                                </div>
-                    </Tooltip>
-                                <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={null}>
-                                    <Flex vertical>
-                                        {/* <Upload
+                                ) : (
+                                    <Avatar
+                                        className=""
+                                        size="large"
+                                        src={pic || <UserOutlined />}
+                                        icon={!pic ? <FaUser /> : undefined}
+                                        alt="avatar"
+                                        onClick={() => setIsModalOpen(true)}
+                                    />
+                                )}
+                            </div>
+                        </Tooltip>
+                        <Modal open={isModalOpen} onOk={handleOk} onCancel={handleCancel} footer={null}>
+                            <Flex vertical>
+                                {/* <Upload
                                                     name="avatar"
                                                     listType="picture-card"
                                                     className="avatar-uploader "
@@ -229,57 +231,50 @@ export default function Header_page() {
                                                         uploadButton
                                                     )}
                                                 </Upload> */}
-                                    <ImgCrop rotationSlider aspectSlider>
-                                        <Upload
-                                            action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
-                                            listType="picture-card"
-                                            className="avatar-uploader "
-                                            fileList={fileList}
-                                            onChange={handleUploadChange}
-                                            onPreview={onPreview}
-                                            beforeUpload={beforeUpload}
-                                        >
-                                            {fileList.length < 1 && <div>{<PlusOutlined />}</div>}
+                                <ImgCrop rotationSlider aspectSlider>
+                                    <Upload
+                                        action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                                        listType="picture-card"
+                                        className="avatar-uploader "
+                                        fileList={fileList}
+                                        onChange={handleUploadChange}
+                                        onPreview={onPreview}
+                                        beforeUpload={beforeUpload}
+                                    >
+                                        {fileList.length < 1 && <div>{<PlusOutlined />}</div>}
 
-                                            {/* <img src={imageUrl} alt="avatar" width={100} height={100} /> */}
-                                        </Upload>
-                                    </ImgCrop>
+                                        {/* <img src={imageUrl} alt="avatar" width={100} height={100} /> */}
+                                    </Upload>
+                                </ImgCrop>
 
-                                    <Input
-                                        size="large"
-                                        placeholder="Username"
-                                        prefix={<UserOutlined />}
-                                        value={name}
-                                        onChange={handleUsernameChange}
-                                    />
-                                    <br></br>
-                                    <Input size="large" placeholder="Password" prefix={<RiLockPasswordLine />} />
-                                    <br></br>
-                                    <div className="flex justify-end mt-4">
-                                        <Button onClick={handleCancel}>
-                                            Cancel
-                                        </Button>
-                                        <Button
-                                            type="primary"
-                                            onClick={handleOk}
-                                            className="bg-blue-500 hover:bg-blue-600"
-                                        >
-                                            OK
-                                        </Button>
-                                    </div>
-                                </Flex>
-                            </Modal>
-                        
+                                <Input
+                                    size="large"
+                                    placeholder="Username"
+                                    prefix={<UserOutlined />}
+                                    value={name}
+                                    onChange={handleUsernameChange}
+                                />
+                                <br></br>
+                                <Input size="large" placeholder="Password" prefix={<RiLockPasswordLine />} />
+                                <br></br>
+                                <div className="flex justify-end mt-4">
+                                    <Button onClick={handleCancel}>Cancel</Button>
+                                    <Button type="primary" onClick={handleOk} className="bg-blue-500 hover:bg-blue-600">
+                                        OK
+                                    </Button>
+                                </div>
+                            </Flex>
+                        </Modal>
+
                         <Tooltip title={POPOVER_LOGOUT}>
                             <Button className="text-xl" onClick={handleLogout} danger>
-                                <BiLogOut className=''/>
+                                <BiLogOut className="" />
                             </Button>
                         </Tooltip>
                     </div>
-                    </Flex>
                 </Flex>
-            </Header>
-
+            </Flex>
+            {/* </Header> */}
         </div>
     );
 }

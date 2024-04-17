13bc74c4bb32550e47cdab02f76086bef5bb0ae3
prit-patel2003 <pritp@solguruz.com>
2024-04-17 15:33:02 +0530

    blog wip

--- src/http/Form_data.ts
+++ src/http/Form_data.ts
@@ -19,7 +19,7 @@ const http = axios.create({
     baseURL: 'http://192.168.68.68:3000',
     headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json',
+        'Content-Type': 'multipart/form-data',
     },
 });
 
